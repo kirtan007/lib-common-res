c70ee9a1dbf9a455aa37e58fd689fa75e7e3caba
Kirtan Patel <kirtan.patel@rapidops.com>
2017-08-13 15:35:55 +0530

    Initial Import

---
+++ app/src/main/java/com/tinymatrix/commonres/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tinymatrix.commonres.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(com.tinymatrix.commonres.R.layout.activity_main);
+    }
+}
